0633936d090f4e98c3679c200dc00a33b1bba7fc
Masunaga Ray <ray@msng.info>
2017-10-17 13:44:41 +0900

    Add isDefaultCard() method to Card

--- src/Entities/Card.php
+++ src/Entities/Card.php
@@ -2,6 +2,8 @@
 
 namespace msng\GmoPaymentGateway\Entities;
 
+use msng\GmoPaymentGateway\Values\Card\DefaultFlag;
+
 class Card extends ResourceEntity
 {
     const SEQ_MODE = 'SeqMode';
@@ -12,4 +14,18 @@ class Card extends ResourceEntity
     const EXPIRE = 'Expire';
     const HOLDER_NAME = 'HolderName';
     const DELETE_FLAG = 'DeleteFlag';
+
+    /**
+     * @return bool
+     */
+    public function isDefaultCard()
+    {
+        $defaultFlag = $this->get(static::DEFAULT_FLAG);
+
+        if ($defaultFlag instanceof DefaultFlag && $defaultFlag->isDefault()) {
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/Values/Card/DefaultFlag.php
+++ src/Values/Card/DefaultFlag.php
@@ -11,4 +11,12 @@ class DefaultFlag extends EnumValue
 
     //継続課金対象とする
     const TRUE = '1';
+
+    /**
+     * @return bool
+     */
+    public function isDefault()
+    {
+        return $this->value === static::TRUE;
+    }
 }
